6411c731263a4d0b282e3213adc08029bb4bd5f6
YIBRAH-MENAN <140635028+YIBRAH-MENAN@users.noreply.github.com>
2023-07-26 18:48:51 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+const btn = document.querySelector('.btn');
+
+const date = document.querySelector('.date');
+const time = document.querySelector('.time');
+const place = document.querySelector('.place');
+const regNumber = document.querySelector('.reg-number');
+
+
+
+
+
+btn.addEventListener("click", () => {
+    if(date.value===""){
+     
+    }else{
+        console.log(date.value, time.value, place.value, regNumber.value)
+    }
+  
+})
+
+
+const btn = document.querySelector('.btn');
+
+
+const ምሉእ ስምe = document.querySelector('.ምሉእ ስም');
+const ደረ/ትምህርቲ = document.querySelector('.ደረ/ትምህርቲ');
+const ስም ኣደ = document.querySelector('.ስም ኣደ');
+const ብሄር = document.querySelector('.ብሄር');
+
+btn.addEventListener("click", () => {
+    if(date.value===""){
+     
+    }else{
+        console.log(ምሉእ ስም.value, ደረ/ትምህርቲ.value, ስም ኣደ.value, ብሄር.value)
+    }
+  
+})
